1a8849702839ab713666b4b767164b51d11977bc
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2022-08-14 06:24:29 +0530

    Update stubs.js

--- src/stubs.js
+++ src/stubs.js
@@ -13,22 +13,41 @@ int main() {
 
 stubs.py =
 `
-n = 10;  
-m = (2 * n) - 2  
-for i in range(0, n):  
-    for j in range(0, m):  
-        print(end=" ")  
-    m = m - 1 
-    for j in range(0, i + 1):  
-        
-        print("* ", end=' ')  
-    print(" ") 
+# A Sample class with init method
+class Person:
+
+	# init method or constructor
+	def __init__(self, name):
+		self.name = name
+
+	# Sample Method
+	def say_hi(self):
+		print('Hello, my name is', self.name);
+		
+	def pattern(self):
+	    n = 10;
+	    m = (2 * n) - 2;
+	    for i in range(0, n):
+	        for j in range(0, m):
+	            print(end=" ")
+	        m = m - 1;
+	        for j in range(0, i + 1):
+	            print("*", end=' ');
+	        print(" ");
+	    
+
+
+p = Person('Sasa Online Compiler')
+p.say_hi();
+p.pattern();
+
+
 
 `;
 
 stubs.java =
 `import java.io.*;
-class UserName1
+class ClassName1
 {
 	public static void printStars(int n)
 	{
