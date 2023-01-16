f59ae7e49baf0fcf72153072ce8850b34af0c418
Sharad Rakesh Maddheshiya <59807761+sharad9@users.noreply.github.com>
2023-01-16 19:34:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import axios from "axios";
 import "./App.css";
-import stubs from "./stubs";
 import React, { useState, useEffect } from "react";
-
-
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import AceEditor from 'react-ace';
-
 import 'brace/mode/python';
 import 'brace/mode/java';
 import 'brace/mode/c_cpp';
-
 import 'brace/ext/searchbox';
 import 'brace/ext/language_tools';
 import 'brace/theme/ambiance';
@@ -29,7 +20,6 @@ import 'brace/theme/kr_theme';
 import 'brace/theme/merbivore';
 import 'brace/theme/mono_industrial';
 import 'brace/theme/monokai';
-
 import 'brace/theme/pastel_on_dark';
 import 'brace/theme/solarized_dark';
 import 'brace/theme/terminal';
@@ -39,8 +29,6 @@ import 'brace/theme/tomorrow_night_bright';
 import 'brace/theme/tomorrow_night_eighties';
 import 'brace/theme/twilight'
 import 'brace/theme/vibrant_ink'
-
-
 import 'brace/theme/chrome';
 import 'brace/theme/clouds';
 import 'brace/theme/crimson_editor';
@@ -52,69 +40,81 @@ import 'brace/theme/iplastic';
 import 'brace/theme/solarized_light';
 import 'brace/theme/textmate';
 import 'brace/theme/tomorrow';
-
 import 'brace/theme/xcode';
 import 'brace/theme/kuroir';
 import 'brace/theme/katzenmilch';
 import 'brace/theme/sqlserver';
-function App() {
+
+import Select from 'react-select';
+
+export default function App() {
 
   const [code, setCode] = useState("");
   const [output, setOutput] = useState("");
-  const [input, setInput] = useState("");
-  const [language, setLanguage] = useState("cpp");
-  const [theme, setTheme] = useState('chaos');
-  const [userName, setUserName] = useState("Main");
+  const [inputs, setInputs] = useState("");
+  const [language, setLanguage] = useState("");
+  const [theme, setTheme] = useState('xcode');
   const [status, setStatus] = useState(null);
+  const userName = new URL(window.location.href).pathname.substring(1,);
   const [executionTimeDispay, setexecutionTimeDispay] = useState(null);
-
-  useEffect(() => {
-    setCode(stubs[language]);
-  }, [language]);
+  const defaultLang = localStorage.getItem("default-language");
+  const defaultCode = localStorage.getItem("default-code");
 
 
   useEffect(() => {
-    const defaultLang = localStorage.getItem("default-language") || "py";
-    setLanguage(defaultLang);
+    defaultLang ?setLanguage(defaultLang):setLanguage('python')
+
+    defaultCode ?setCode(defaultCode):setCode('')
+
   }, []);
-
-  const languageDict = {
-    py: 'python',
-    java: 'java',
-    cpp: 'c_cpp'
+  const handleLanguageChange = async (e) => {
+    setStatus("");
+    setLanguage(e.value);
   }
+  
+  const languageDict = {
+    python: 'Python',
+    java: 'Java',
+    cpp: 'Cpp'
+  };
 
+  const languages = [{ value: "python", label: "Python" }, { value: "java", label: "Java" }]
+ 
   const handleSubmit = async () => {
 
+    let formatInputs = []
+    for (let str of inputs.split('\n')) {
+      formatInputs.push(str);
+    }
+
     const payload = {
-      language,
-      code,
-      userName,
-      input
+      language:languageDict[language],
+      code:code,
+      userName:userName,
+      inputs: formatInputs
     };
+   
 
     try {
       setOutput("");
-      setInput(input);
+      setCode(code);
       
       setStatus("Submitted");
       setexecutionTimeDispay(null);
-      const { data } = await axios.post("http://localhost:5000/run", payload);
+      
+      const { data } = await axios.post("http://127.0.0.1:8000/runcode", payload);
       setStatus("Executed");
-
       console.log(data);
-
-
-      setUserName(userName);
       setexecutionTimeDispay(data.executionTime);
-
-
-
-      setOutput('Output: \n' + data.result.output.stdout + '\n' + 'Status: \n' + data.result.output.stderr);
+      let output = "";
+      for(let str of data.output){
+        output += str+'\n'
+      }
+      setOutput(output);
 
     } catch ({ response }) {
       if (response) {
-        const errMsg = response.data.err.stderr;
+        const errMsg = response.data.stderr;
         setOutput(errMsg);
       } else {
         setOutput("Please retry submitting.");
@@ -122,185 +122,76 @@ function App() {
     }
   };
 
-  const setDefaultLanguage = () => {
+  const setDefault = () => {
     localStorage.setItem("default-language", language);
-    console.log(`${language} set as default!`);
+    localStorage.setItem("default-code", code);
+    alert("Saved Your Code");
   };
-
-
-  function validatePassword(p) {
-    const errors = [];
-    var specials = /[^A-Za-z 0-9]/g;
-
-    if (specials.test(p)) {
-      errors.push("Your class name should only contain Letters And Digits.");
-    } else
-      if (p[0].search(/[A-Z]/) < 0) {
-        errors.push("Your class name must start with one upper case letter.");
-      } else
-
-        if (p.length < 1) {
-          errors.push("Your class name must be at least 1 characters");
-        } else
-          if (p.length > 32) {
-            errors.push("Your class name must be at max 32 characters");
-          } 
-
-    if (errors.length > 0) {
-      document.getElementById('userNameStatus').innerText = errors[0];
-      errors.shift();
-      console.log(errors.join("\n"));
-      return false;
-    }
-    document.getElementById('userNameStatus').innerText = "You Are Ready To Proceed.";
-    return true;
-  }
-
-
-  function onChangeCode(newValue) {
-
+  
+  const onChangeCode =(newValue)=> {
     setCode(newValue);
     
-
   }
-  function onChangeInput(newValue){
-    setInput(newValue);
+
+  const onChangeInput =(newValue)=> {
+    setInputs(newValue);
   }
 
   return (
     <div className="App" >
-
-
       <nav role="navigation" class="navbar navbar-default">
-        <ul class="nav nav-pills pull-left">
-          <form>
-            <img src="./favicon.ico" alt="Avatar" width="30" height="20" class="img-rounded"></img>
-            <label>Sasa Online Compiler</label>
-            <button type="button" class="btn btn-danger btn-xs"
-              id="status">Login</button>
-              
-          </form>
-          
-        </ul>
-
+        
         <div class="pull-right">
-
+          <span class="">Set Theme</span>
           <input
             onChange={(e) => { document.getElementsByClassName("App")[0].style = `background:${e.target.value}` }}
             defaultValue='#1E2152' type="color" id="colorpicker" />
-
         </div>
-        
-          <span class=""></span>
-          
-        
+    
         <input type="button" 
           onClick={(e) => { if (e.target.value == "SHOW MENU ↓") { e.target.value = "HIDE MENU ↑" } else { e.target.value = "SHOW MENU ↓"  }}}
-        
           class="btn-block" data-toggle="collapse" data-target="#toggleDemo" value="HIDE MENU ↑" />
-        
-
-
-
-
-
-        
-
+          
       </nav>
       <div class="bs-example">
-
-       
-
           <div id="toggleDemo" class="collapse in">
           <div class='container'>
             <a class="btn btn-block btn-default btn-xs" href="#">
               <h4>
                 <span class="glyphicon glyphicon-th-large"></span>
-                <span class="glyphicon-class">Sasa Coding Problems</span>
+                <span class="glyphicon-class"><b> code It compile It</b></span>
               </h4>
-
             </a>  
           </div>  
           <div class="container">
-
             <div class="panel panel-default">
               <div class='panel-body'>
-                
                 <div class="row">
-              
-
                   <div class="col-md-4">
-                    <a class="btn btn-primary btn-sm" onClick={setDefaultLanguage}>Set This Language As Default</a>
-
-
+                    <a class="btn btn-default btn-sm" style={{ border: "2px solid black" }} onClick={setDefault}>Save Changes</a>
                   </div>
                   <div class="col-md-4">
                     <div class="btn btn-default btn-sm dropdown-toggle">
-                    <select
-                      class="bg-default"
-                      value={language}
+                  
+                      <Select
+                        defaultValue={languages.find((lang)=> lang.value == defaultLang)}
+                        onChange={handleLanguageChange}
+                        options={languages}
+                      />
 
-                      onChange={(e) => {
-                        const shouldSwitch = window.confirm(
-                          "Are you sure you want to change language? WARNING: Your current code will be lost."
-                        );
-                        if (shouldSwitch) {
-                          setStatus("");
-
-                          setLanguage(e.target.value);
-                        }
-                      }}
-
-
-                    >
-
-                      <option value="cpp">C++</option>
-                      <option value="py">Python</option>
-                      <option value="java">Java</option>
-                    </select>
                     </div>
                   </div>
-                  
-
-                  <div class="col-md-4">
-                    
-                    <label>Java Main Class Name:</label>
-
-                    <input
-                      defaultValue={userName}
-                      onChange={(e) => {
-                        const button = document.getElementById('submit');
-                        if (validatePassword(e.target.value)) { button.className = 'btn btn-success btn-sm'; setUserName(e.target.value); } else button.className = 'btn btn-success btn-sm disabled';
-
-                      }}></input>
-
-                    <h6><code class="text-danger" id="userNameStatus">Create Unique <b>Class Name</b></code></h6>
-
-                  </div>
-
-
-
-
                 </div>
-
               </div>
-             
             </div>
-
             <div class="panel panel-default">
               <div class="panel-body">
                 <div class='row'>
-                 
                   <div class="col-md-2">
                     <div class="btn btn-default btn-sm dropdown-toggle">
                       <select id="-theme"
-
                         value={theme}
-
-                        onChange={(e) => { setTheme(e.target.value); }}
-                      >
-
-
+                        onChange={(e) => { setTheme(e.target.value); }}>
                         <optgroup label="Bright">
                           <option value="chrome">Chrome</option>
                           <option value="clouds">Clouds</option>
@@ -343,45 +234,30 @@ function App() {
                           <option value="vibrant_ink">Vibrant Ink</option>
                         </optgroup>
                       </select>
-
-
-
                     </div>
-
                   </div>
-
                   <div class="col-md-4">
-                   
                     <a id="submit" onClick={handleSubmit} class="btn btn-success btn-block btn-sm" >Run</a>
-
-
                   </div>
                   <div class="col-md-5">
-                  
                     <strong>Status : </strong>{status}
                     {'\t\t'}
                     <strong>{executionTimeDispay}</strong>
                   </div>
                 </div>
               </div>
-
             </div>
-
           </div>
           </div>
         </div>  
           <div class="panel panel-default">
             <div class="panel-body">
-
               <div class="row">
-
                 <div class="col-sm-6 col-md-8 col-lg-7">
                   <div class="editor input">
-
                     <AceEditor
-
                       placeholder={"Class Should Not Be Public And It Should Be Same As Your UserName."}
-                      mode={languageDict[language]}
+                      mode={language}
                       theme={theme}
                       fontSize={16}
                       onChange={onChangeCode}
@@ -391,7 +267,6 @@ function App() {
                       showPrintMargin={true}
                       showGutter={true}
                       highlightActiveLine={true}
-
                       setOptions={{
                         enableBasicAutocompletion: true,
                         enableLiveAutocompletion: true,
@@ -400,63 +275,49 @@ function App() {
                         tabSize: 4,
                       }}
                       name="UNIQUE_ID_OF_DIV3"
-
                     />
                   </div>
                 </div>
-
-
                 <div class="col-sm-6 col-md-3 col-lg-5">
                   <div class="editor output">
-                <AceEditor
-
-                  
-                  mode={languageDict[language]}
-                  theme={theme}
-                  fontSize={16}
-                  onChange={(e)=>{e.target.value=output}}
-                  value={output}
-                  width="100%"
-                  height="285px"
-                  showPrintMargin={true}
-                  showGutter={true}
-                  highlightActiveLine={true}
-               
-                  setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: false,
-                    showLineNumbers: true,
-                    readOnly:true,
-                    tabSize: 4,
-                  }}
-                  name="UNIQUE_ID_OF_DIV2"
-
-
-                />
-                   
-                  </div>
-
+                  <AceEditor
+                    mode={languageDict[language]}
+                    theme={theme}
+                    fontSize={16}
+                    onChange={(e)=>{e.target.value=output}}
+                    value={output}
+                    width="100%"
+                    height="285px"
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    setOptions={{
+                      enableBasicAutocompletion: true,
+                      enableLiveAutocompletion: true,
+                      enableSnippets: false,
+                      showLineNumbers: true,
+                      readOnly:true,
+                      tabSize: 4,
+                    }}
+                    name="UNIQUE_ID_OF_DIV2"
+                  />
                 </div>
-             
+              </div>
             <div class="col-sm-6 col-md-3 col-lg-5">
               <span>Input ↓</span>
               <div class="editor">
                 <AceEditor
-
-
                   placeholder={"Your Input Here"}
                   mode={languageDict[language]}
                   theme={theme}
                   fontSize={16}
                   onChange={onChangeInput}
-                  value={input}
+                  value={inputs}
                   width="100%"
                   height="245px"
                   showPrintMargin={true}
                   showGutter={true}
                   highlightActiveLine={true}
-
                   setOptions={{
                     enableBasicAutocompletion: false,
                     enableLiveAutocompletion: false,
@@ -465,27 +326,12 @@ function App() {
                     tabSize: 4,
                   }}
                   name="UNIQUE_ID_OF_DIV1"
-
-
                 />
-
-              </div>
-
-            </div>
-
               </div>
             </div>
-            
           </div>
-
-
-
+        </div>
       </div>
-
-
-
-
-      );
+    </div>
+  );
 }
-
-      export default App;
